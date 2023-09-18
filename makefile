eaff730a7d4b94d24b8865170abe10b35dc30680
Mika Heares <mheares@idea11.com.au>
2023-09-18 23:41:34 +1000

    added helpers and screen trigger

--- makefile
+++ makefile
@@ -4,9 +4,17 @@ clone:
 install:
 	python3 -m pip install -r requirements.txt
 
+setup-pi:
+	sudo apt-get install python3-scipy
+
 run:
 	cd src/mysite && python3 manage.py runserver 0.0.0.0:8000
 
 listen:
 	python3 src/mic/listener.py
 
+pic:
+	libcamera-still -o image.jpg
+
+copy:
+	scp mhear22@192.168.20.93:/home/mhear22/image.jpg .
--- src/webapp/doot/views.py
+++ src/webapp/doot/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import TemplateHTMLRenderer
+import subprocess
 
 
 class DootModel(APIView):
@@ -16,3 +17,13 @@ class DootModel(APIView):
     def post(self, request, format=None):
         print('Hit')
         return Response({'body': 'done!'})
+    
+    def photo(self, request, format=None):
+        try:
+            cmd = "raspistill -vf -o /home/pi/pitraincamera/images/pic.jpeg"
+            subprocess.call(cmd, shell=True)
+        except:
+            print("Failed to trigger camera")
+        return Response({"body": 'photo taken'})
+    
+    
